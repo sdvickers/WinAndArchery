c825a0b472d3ee34a7ebb0425d5ea352c4a59222
Steve Vickers <stevev1970@gmail.com>
2020-11-28 22:27:54 -0500

    home page commeplte with login modal

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+// change navbar to black on scroll
+
+window.addEventListener('scroll', function () {
+
+        let header = document.querySelector('nav');
+        console.log(header);
+        let header2 = document.getElementsByClassName('dropdown-menu');
+        let windowPosition = window.scrollY > 50;
+
+        header.classList.toggle('scrolling-active', windowPosition);
+        for (i = 0; i < header2.length; i++) {
+            // console.log(header2[i].innerHTML);
+            header2[i].classList.toggle('scrolling-active', windowPosition);
+        }
+    });
+
+   
